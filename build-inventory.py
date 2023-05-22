cf49a31869e8693d550cb242dea4d5c59a38ad81
Unnikrishnan R <unnikrishnan.chaithanya@gmail.com>
2023-05-22 20:58:02 +0530

    updated prometheus config

--- build-inventory.py
+++ build-inventory.py
@@ -11,10 +11,16 @@ ouput = out.decode('utf-8')
 data = json.loads(ouput)
 ips = data['vm_ip']['value']
 #filename = 'modules/' + sol +  '/hosts.ini'
+var_filename = 'hosts.yml'
 filename = 'hosts.ini'
 print(filename)
 with open(filename,'w') as fh:
     for ip in ips:
         fh.write(ip.rstrip() + '\n')
+
+with open(var_filename,'w') as fh:
+    fh.write('hg:' + '\n')
+    for ip in ips:
+        fh.write(' -' + ip.rstrip() + '\n')
 
 
