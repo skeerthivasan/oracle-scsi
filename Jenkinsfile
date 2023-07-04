70e82224844a82abd823f3d34976ae9ff116a665
Unnikrishnan R <unnikrishnan.chaithanya@gmail.com>
2023-07-04 23:18:34 +0530

    update window base module

--- Jenkinsfile
+++ Jenkinsfile
@@ -64,7 +64,7 @@ pipeline {
             def total_count = vm_count.toInteger() + count.toInteger()
             println total_count
             sh script: "$tf_cmd apply -auto-approve -var-file=$path"  + "/main.tfvars" + " -var vsphere_password=" + '${VC_PASS}'	 + " -var ansible_key=" + '${SSH_KEY}'	+	 " -var infoblox_pass=" + '${INFOBLOX_PASS}'  +	" -var vm_count=" + total_count
-//            sh script: "python3 ../../build-inventory.py " + sol.trim()
+            sh script: "python3 ../../build-inventory.py " + sol.trim()
             sh script: "cat hosts.ini"
         }
         if (params.Install) {
--- build-inventory.py
+++ build-inventory.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 import os
 print(os.getcwd())
-
+sol = sys.argv[1]
 process = subprocess.Popen(["terraform", "output", "-json"],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 out, err = process.communicate()
 ouput = out.decode('utf-8')
@@ -14,13 +14,32 @@ ips = data['vm_ip']['value']
 var_filename = 'hosts.yml'
 filename = 'hosts.ini'
 print(filename)
-with open(filename,'w') as fh:
-    for ip in ips:
-        fh.write(ip.rstrip() + '\n')
 
-with open(var_filename,'w') as fh:
-    fh.write('hg:' + '\n')
-    for ip in ips:
-        fh.write(' - ' + ip.rstrip() + '\n')
+# check if  the solution is windows
+# prepare the hosts.ini with more details to login 
+if sol == 'MSSQL':
+    print(sol)
+    with open(filename,'w') as fh:
+        fh.write("[win]")
+        for ip in ips:
+            fh.write(ip.rstrip() + '\n')
+        fh.write("[win:vars]")
+        fh.write("ansible_user=administrator")
+        fh.write("ansible_password=VMware1!")
+        fh.write("ansible_connection=winrm")
+        fh.write("ansible_winrm_server_cert_validation=ignore")
+        fh.write("ansible_port=5985")
+        fh.write("ansible_winrm_scheme=http")
+        fh.write("ansible_winrm_kerberos_delegation=true")
+
+else:
+    with open(filename,'w') as fh:
+        for ip in ips:
+            fh.write(ip.rstrip() + '\n')
+
+    with open(var_filename,'w') as fh:
+        fh.write('hg:' + '\n')
+        for ip in ips:
+            fh.write(' - ' + ip.rstrip() + '\n')
 
 
