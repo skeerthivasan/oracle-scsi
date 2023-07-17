df2b5a8f13b98c116f0fb973e881e1c8606ae07d
Unnikrishnan R <unnikrishnan.chaithanya@gmail.com>
2023-07-17 15:12:47 +0530

    update oracle module

--- modules/Oracle/main.tf
+++ modules/Oracle/main.tf
@@ -1,5 +1,5 @@
 module "vm" {
-    source = "../basemodule-oracle"
+    source = "../basemodule"
     vmware_os_template               = var.vmware_os_template
     osguest_id                       = var.osguest_id
     internal_domain                  = var.internal_domain
