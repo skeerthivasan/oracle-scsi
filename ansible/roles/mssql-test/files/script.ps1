cc3194f441b6a31f55914f3e6c78d5a6965b769a
Unnikrishnan R <unnikrishnan.chaithanya@gmail.com>
2023-08-08 21:42:58 +0530

    updated mssql

--- ansible/roles/mssql-test/files/script.ps1
+++ ansible/roles/mssql-test/files/script.ps1
@@ -1,3 +1,3 @@
 Import-Module 'C:\Program Files\Pure Storage\D.O.E.S\DOES.Powershell.dll'
 
-Add-DataEngine -DatabaseType MicrosoftSQL -Hostname localhost -DatabaseName db1 -Amount 1 -Unit Terabytes -NumberOfThreads 8 -UserName admin -Password VMware1! -Folder \\10.21.152.167\packages\datafiles\DataEngineFilesOracle1
+Add-DataEngine -DatabaseType MicrosoftSQL -Hostname localhost -DatabaseName db1 -Amount 1 -Unit Terabytes -NumberOfThreads 8 -UserName admin -Password VMware1! -Folder \\fileserverdb\datafiles\DataEngineFilesOracle1
