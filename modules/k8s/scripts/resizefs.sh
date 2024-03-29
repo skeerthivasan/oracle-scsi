d16f1da27ceca41e741abe10e6705b99ffcc8751
Unnikrishnan R <unnikrishnan.chaithanya@gmail.com>
2024-03-29 18:01:54 +0530

    k8s resize script

--- modules/k8s/scripts/resizefs.sh
+++ modules/k8s/scripts/resizefs.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 /usr/sbin/pvresize /dev/sdb
-/sbin/lvextend -L +120G -r /dev/mapper/sysvg-root
-/sbin/lvextend -L +20G -r /dev/mapper/sysvg-var
-/sbin/lvextend -L +20G -r /dev/mapper/sysvg-opt
-/sbin/lvextend -L +20G -r /dev/mapper/sysvg-tmp
-/sbin/lvextend -L +10G -r /dev/mapper/sysvg-log
-/sbin/lvextend -L +10G -r /dev/mapper/sysvg-home
+/sbin/lvextend -L +200G -r /dev/mapper/sysvg-root
 
