c4baeda57ed494b129ef275341b3486de8bba5a4
Unnikrishnan R <unnikrishnan.chaithanya@gmail.com>
2024-09-11 10:15:55 +0530

    updated

--- modules/spark/scripts/resizefs.sh
+++ modules/spark/scripts/resizefs.sh
@@ -37,6 +37,34 @@ if [ "$OS" = "Linux" ]; then
         rhel)
             echo "Running on Red Hat Enterprise Linux"
             # Commands specific to RHEL
+            /usr/sbin/pvresize /dev/sdb
+            /sbin/lvextend -L +10G -r /dev/mapper/sysvg-lv_root
+            /sbin/lvextend -L +20G -r /dev/mapper/sysvg-lv_home
+            /sbin/lvextend -L +10G -r /dev/mapper/sysvg-lv_var
+            /sbin/lvextend -L +15G -r /dev/mapper/sysvg-lv_log
+            /sbin/lvextend -L +15G -r /dev/mapper/sysvg-lv_audit
+            /sbin/lvextend -L +10G -r /dev/mapper/sysvg-lv_tmp 
+            /sbin/lvextend -L +5G -r /dev/mapper/sysvg-lv_opt 
+            /sbin/swapoff /dev/mapper/sysvg-lv_swap
+            /sbin/lvextend -L +10G  /dev/mapper/sysvg-lv_swap
+            /usr/sbin/mkswap /dev/mapper/sysvg-lv_swap
+            /sbin/swapon /dev/mapper/sysvg-lv_swap
+            ;;
+        rocky)
+            echo "Running on Rocky Linux"
+            # Commands specific to Rocky
+            /usr/sbin/pvresize /dev/sdb
+            /sbin/lvextend -L +10G -r /dev/mapper/sysvg-lv_root
+            /sbin/lvextend -L +20G -r /dev/mapper/sysvg-lv_home
+            /sbin/lvextend -L +10G -r /dev/mapper/sysvg-lv_var
+            /sbin/lvextend -L +15G -r /dev/mapper/sysvg-lv_log
+            /sbin/lvextend -L +15G -r /dev/mapper/sysvg-lv_audit
+            /sbin/lvextend -L +10G -r /dev/mapper/sysvg-lv_tmp 
+            /sbin/lvextend -L +5G -r /dev/mapper/sysvg-lv_opt 
+            /sbin/swapoff /dev/mapper/sysvg-lv_swap
+            /sbin/lvextend -L +10G  /dev/mapper/sysvg-lv_swap
+            /usr/sbin/mkswap /dev/mapper/sysvg-lv_swap
+            /sbin/swapon /dev/mapper/sysvg-lv_swap
             ;;
         *)
             echo "Unknown Linux distribution: $DISTRO"
