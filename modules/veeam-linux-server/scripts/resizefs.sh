e2e54041d154eb4bafd9852401ce940d17cb7848
Anil Dahiya <andahiya@purestorage.com>
2024-11-11 17:23:22 +0530

    Linux swapresize fix

--- modules/veeam-linux-server/scripts/resizefs.sh
+++ modules/veeam-linux-server/scripts/resizefs.sh
@@ -7,7 +7,7 @@
 /sbin/lvextend -L +15G -r /dev/mapper/sysvg-lv_audit
 /sbin/lvextend -L +10G -r /dev/mapper/sysvg-lv_tmp 
 /sbin/lvextend -L +5G -r /dev/mapper/sysvg-lv_opt 
-/sbin/swapoff /dev/mapper/sysvg-lv_swap
-/sbin/lvextend -L +10G  /dev/mapper/sysvg-lv_swap
+#/sbin/swapoff /dev/mapper/sysvg-lv_swap
+#/sbin/lvextend -L +10G  /dev/mapper/sysvg-lv_swap
 /usr/sbin/mkswap /dev/mapper/sysvg-lv_swap
 /sbin/swapon /dev/mapper/sysvg-lv_swap
